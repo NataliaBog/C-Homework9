603c637de391382518e4c1571172eefbe382c2d9
Natalia <natashaaa60802@rambler.ru>
2022-09-04 13:19:21 +0500

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine();
+
+            Console.WriteLine("Введите натуральное число M:");
+
+            bool numberCheck = int.TryParse(Console.ReadLine(), out int task64_start);
+            while (!numberCheck)
+            {
+                Console.WriteLine("Введено не число. Повторите ввод:");
+                numberCheck = int.TryParse(Console.ReadLine(), out task64_start);
+            }
+
+            // считано число M
+
+            Console.WriteLine("Введите натуральное число N:");
+
+            numberCheck = int.TryParse(Console.ReadLine(), out int task64_end);
+            while (!numberCheck)
+            {
+                Console.WriteLine("Введено не число. Повторите ввод:");
+                numberCheck = int.TryParse(Console.ReadLine(), out task64_end);
+            }
+
+            // считано число N
+
+
+            if (task64_end < task64_start)
+                for (int i = task64_start; i >= task64_end; i--)
+                {
+                    {
+                        if (i < task64_start) Console.Write(", ");
+                        Console.Write(i);
+                    }
+                }
+            else
+                for (int i = task64_start; i <= task64_end; i++)
+                {
+                    {
+                        if (i > task64_start) Console.Write(", ");
+                        Console.Write(i);
+                    }
+                }
+
+            Console.WriteLine("\n");
